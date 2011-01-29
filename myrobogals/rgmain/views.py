3b01e76b290cf353eeea71a254b398e1a467fe2d
Mark Parncutt <me@markparncutt.com>
2011-01-29 12:33:42 +0000

    Commit to new repos
    git-svn-id: file:///home/myrobogals/repos/robogals@55 65fac5d3-f2cb-df11-bcad-000c2977486c

--- myrobogals/rgmain/views.py
+++ myrobogals/rgmain/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from myrobogals.auth.models import Group
 
+
 def home(request):
 	return render_to_response('home.html', {}, context_instance=RequestContext(request))
 
