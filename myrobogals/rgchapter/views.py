2d5541c83e22a4859a804bfb367b6fe319f2cf7c
Hamish Graham <graham.hamish@gmail.com>
2011-05-05 15:29:41 +0000

    Fixed 500 error
    git-svn-id: file:///home/myrobogals/repos/robogals@113 65fac5d3-f2cb-df11-bcad-000c2977486c

--- myrobogals/rgchapter/views.py
+++ myrobogals/rgchapter/views.py
@@ -14,13 +14,22 @@ def list(request):
 	listing = []
 	superchapters = Group.objects.filter(parent=1)		
 	for superchapter in superchapters:
-		if superchapter.status == 0 or superchapter == request.user.chapter() or request.user.is_superuser or request.user.chapter().parent == superchapter:
-			chapters_all = Group.objects.filter(parent=superchapter)
-			chapters_display = []
-			for chapter in chapters_all:
-				if chapter.status == 0 or chapter == request.user.chapter() or request.user.is_superuser:
-					chapters_display.append(chapter)
-			listing.append({'super': superchapter, 'chapters': chapters_display})
+		if request.user.is_authenticated():
+			if superchapter.status == 0 or superchapter == request.user.chapter() or request.user.is_superuser or request.user.chapter().parent == superchapter:
+				chapters_all = Group.objects.filter(parent=superchapter, status=0)
+				chapters_display = []
+				for chapter in chapters_all:
+					if chapter.status == 0 or chapter == request.user.chapter() or request.user.is_superuser:
+						chapters_display.append(chapter)
+				listing.append({'super': superchapter, 'chapters': chapters_display})
+		else:
+			if superchapter.status == 0:
+				chapters_all = Group.objects.filter(parent=superchapter)
+				chapters_display = []
+				for chapter in chapters_all:
+					if chapter.status == 0:
+						chapters_display.append(chapter)
+				listing.append({'super': superchapter, 'chapters': chapters_display})
 	return render_to_response('chapter_listing.html', {'listing': listing}, context_instance=RequestContext(request))
 
 def joinlist(request):
