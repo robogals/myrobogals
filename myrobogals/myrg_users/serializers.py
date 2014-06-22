be87046798e5c130b8accc4176c0f69457099808
manhinli <manhinli@gmail.com>
2014-06-22 08:02:11 +0000

    Removing old Groups reference in serializer

--- myrobogals/myrg_users/serializers.py
+++ myrobogals/myrg_users/serializers.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import Group
 from .models import RobogalsUser
 
 from rest_framework import serializers
@@ -8,8 +7,3 @@ class RobogalsUserSerializer(serializers.ModelSerializer):
         model = RobogalsUser
         #fields = ('url', 'username', 'given_name', 'primary_email', 'groups', 'is_active', 'is_superuser',)
         write_only_fields = ('password',)
-        
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ('url', 'name')
