04f082df0b66cd144f18cf9af355a51d8f3b6c85
Mark Parncutt <me@markparncutt.com>
2020-08-18 00:32:17 +1000

    Added sample data for developers

--- myrobogals/__init__.py
+++ myrobogals/__init__.py
@@ -1,6 +1,17 @@
 # Required for running myRobogals on newer versions of macOS
-
 import os
 if os.uname()[0] == 'Darwin':
     import pymysql
     pymysql.install_as_MySQLdb()
+
+# Allow loading of sample data to SQLite
+import sys
+for cmdarg in sys.argv:
+    if cmdarg == 'loaddata':
+        from django.db.backends.signals import connection_created
+        def activate_foreign_keys(sender, connection, **kwargs):
+            if connection.vendor == 'sqlite':
+                cursor = connection.cursor()
+                cursor.execute('PRAGMA foreign_keys = OFF;')
+                print("Disabled foreign key checks while importing fixtures")
+        connection_created.connect(activate_foreign_keys)
